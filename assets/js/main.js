c1fc1461e0ab62340c61456a87b7604a98c4ee20
LIHTU <robinanelson@gmail.com>
2016-12-16 17:12:30 -0800

    added working circles with randomly selected arc widths 1 || 4.

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,6 +27,7 @@ function canvasApp () {
         ctx.fillRect(0, 0, cWidth, cHeight);
 
         // grid
+        ctx.lineWidth = 1;
         var xPen = 0;
         while (xPen < cWidth) {
             xPen += 64;
@@ -47,6 +48,37 @@ function canvasApp () {
             ctx.stroke();
         }
 
+        var centerX = 0;
+        var centerY = 0;
+        var inc = 128;
+        var radius = inc/4.0;
+        var sArc = 0;
+        var eArc = 0.5*Math.PI;
+        var arcStrokes = [1,4];
+        var quadrant = 0;
+
+        while (centerX < cWidth + inc) {
+            while (centerY < cHeight + inc) {
+                while (quadrant <= 3) {
+                    ctx.lineWidth = arcStrokes[Math.floor(Math.random()+0.5)];
+                    console.log(ctx.lineWidth);
+                    ctx.beginPath()
+                    ctx.arc(centerX, centerY, radius, sArc, eArc);
+                    ctx.stroke()
+                    quadrant++;
+                    sArc += 0.5 * Math.PI;
+                    eArc += 0.5 * Math.PI;
+                }
+                centerY += inc;
+                quadrant = 0;
+                sArc = 0;
+                eArc = 0.5*Math.PI;
+            }
+            centerX += inc;
+            centerY = 0;
+        }
+
+
 
 	}
 
@@ -54,9 +86,22 @@ function canvasApp () {
     var spaceBubbsImg = new Image();
     spaceBubbsImg.src = "images/space_bubbs.jpg"; // Change from previously listed code; image src location is different
 
+    var CircOb = function() {
+        this.rad = 24;
+    }
+
+    CircOb.prototype.get_rad = function() {
+        return this.rad;
+    }
+
+
+
+
+
+
     //box
     function gameLoop() {
-        window.setTimeout(gameLoop, 40);
+        window.setTimeout(gameLoop, 400);
         drawScreen();
     }
 
